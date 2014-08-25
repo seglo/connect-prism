c3a44e9f29338377742aa14cd45f68ae5735a81b
seglo <seglo@randonom.com>
2014-08-25 19:36:50 -0400

    Turned Prism into a type

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
+var di = require('di');
+
+var Prism = require('./lib/prism');
+
+var injector = new di.Injector([]);
+
+var p = injector.get(Prism);
+
 module.exports = {
-  create: require('./lib/prism'),
+  create: p.create,
   middleware: require('./lib/events').handleRequest
 };
