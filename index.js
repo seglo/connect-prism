60913bc83fcf791b66730061c1e760321c0077f3
seglo <seglo@randonom.com>
2014-08-25 20:22:34 -0400

    Created HttpEvents type. Updated entrypoint to use injector

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 var di = require('di');
 
 var Prism = require('./lib/prism');
+var HttpEvents = require('./lib/http-events');
 
 var injector = new di.Injector([]);
 
-var p = injector.get(Prism);
-
 module.exports = {
-  create: p.create,
-  middleware: require('./lib/events').handleRequest
+  create: injector.get(Prism).create,
+  middleware: injector.get(HttpEvents).handleRequest
 };
