49925e7f739133e31383a1982816826de1fa80c4
Ubu <ubald2b@gmail.com>
2018-12-02 17:20:50 +0100

    Response delay - Formatting : remove space between anonymous function and parameter

--- test/unit/response-delay.spec.js
+++ test/unit/response-delay.spec.js
@@ -4,23 +4,23 @@ var assert = require('assert');
 
 var ResponseDelay = require('../../lib/services/response-delay');
 
-describe('Response delay', function () {
+describe('Response delay', function() {
   var responseDelay;
 
-  beforeEach(function () {
+  beforeEach(function() {
     responseDelay = new ResponseDelay();
   });
 
-  describe('No delay', function () {
-    it('should have no delay by default', function () {
+  describe('No delay', function() {
+    it('should have no delay by default', function() {
       assert.equal(responseDelay.delayTimeInMs(), 0);
     });
 
-    it('should have no delay when given null parameter', function () {
+    it('should have no delay when given null parameter', function() {
       assert.equal(responseDelay.delayTimeInMs(null), 0);
     });
 
-    it('should have no delay when given a parameter that coerce to false', function () {
+    it('should have no delay when given a parameter that coerce to false', function() {
       assert.equal(responseDelay.delayTimeInMs(false), 0);
       assert.equal(responseDelay.delayTimeInMs(0), 0);
       assert.equal(responseDelay.delayTimeInMs(''), 0);
@@ -28,70 +28,70 @@ describe('Response delay', function () {
     });
   });
 
-  describe('Simple delays', function () {
-    it('should have a simple delay when given a number', function () {
+  describe('Simple delays', function() {
+    it('should have a simple delay when given a number', function() {
       assert.equal(responseDelay.delayTimeInMs(42), 42);
     });
 
-    it('should have a simple delay when given a string that coerce to a number', function () {
+    it('should have a simple delay when given a string that coerce to a number', function() {
       assert.equal(responseDelay.delayTimeInMs('42'), 42);
     });
   });
 
-  describe('Ranged delays', function () {
-    describe('Lower range', function () {
-      beforeEach(function () {
-        var fakeRandomNumberGenerator = function () {
+  describe('Ranged delays', function() {
+    describe('Lower range', function() {
+      beforeEach(function() {
+        var fakeRandomNumberGenerator = function() {
           return 0;
         };
         responseDelay = new ResponseDelay(fakeRandomNumberGenerator);
       });
 
-      it('should have minimum delay of 500ms when using auto parameter', function () {
+      it('should have minimum delay of 500ms when using auto parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('auto'), 500);
       });
 
-      it('should have minimum delay of 150ms when using fast parameter', function () {
+      it('should have minimum delay of 150ms when using fast parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('fast'), 150);
       });
 
-      it('should have minimum delay of 1500ms when using slow parameter', function () {
+      it('should have minimum delay of 1500ms when using slow parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('slow'), 1500);
       });
 
-      it('should have minimum delay of 1ms when given an invalid parameter', function () {
+      it('should have minimum delay of 1ms when given an invalid parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('invalid parameter'), 1);
       });
     });
 
-    describe('Upper range', function () {
-      beforeEach(function () {
-        var fakeRandomNumberGenerator = function () {
+    describe('Upper range', function() {
+      beforeEach(function() {
+        var fakeRandomNumberGenerator = function() {
           return 1;
         };
         responseDelay = new ResponseDelay(fakeRandomNumberGenerator);
       });
 
-      it('should have maximum delay of 1750ms when using auto parameter', function () {
+      it('should have maximum delay of 1750ms when using auto parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('auto'), 1750);
       });
 
-      it('should have maximum delay of 1000ms when using fast parameter', function () {
+      it('should have maximum delay of 1000ms when using fast parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('fast'), 1000);
       });
 
-      it('should have maximum delay of 3000ms when using slow parameter', function () {
+      it('should have maximum delay of 3000ms when using slow parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('slow'), 3000);
       });
 
-      it('should have maximum delay of 1ms when given an invalid parameter', function () {
+      it('should have maximum delay of 1ms when given an invalid parameter', function() {
         assert.equal(responseDelay.delayTimeInMs('invalid parameter'), 1);
       });
     });
 
     describe('Custom ranges', function() {
-      it('should use the given upper bound to calculate the delay', function () {
-        var fakeRandomNumberGenerator = function () {
+      it('should use the given upper bound to calculate the delay', function() {
+        var fakeRandomNumberGenerator = function() {
           return 1;
         };
         responseDelay = new ResponseDelay(fakeRandomNumberGenerator);
@@ -99,8 +99,8 @@ describe('Response delay', function () {
         assert.equal(responseDelay.delayTimeInMs({lowerBound: 50, upperBound: 100}), 100);
       });
 
-      it('should use the given lower bound to calculate the delay', function () {
-        var fakeRandomNumberGenerator = function () {
+      it('should use the given lower bound to calculate the delay', function() {
+        var fakeRandomNumberGenerator = function() {
           return 0;
         };
         responseDelay = new ResponseDelay(fakeRandomNumberGenerator);
@@ -109,8 +109,8 @@ describe('Response delay', function () {
       });
     });
 
-    it('should round delays to the millisecond', function () {
-      var fakeRandomNumberGenerator = function () {
+    it('should round delays to the millisecond', function() {
+      var fakeRandomNumberGenerator = function() {
         return 0.001;
       };
       responseDelay = new ResponseDelay(fakeRandomNumberGenerator);
