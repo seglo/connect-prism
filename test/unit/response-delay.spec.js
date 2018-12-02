e5f70cd1e0aa294ad91d7850646210cf5af85482
Ubu <ubald2b@gmail.com>
2018-12-02 16:56:52 +0100

    Response delay - Refactoring to use assert.equal for better error messages when tests fail

--- test/unit/response-delay.spec.js
+++ test/unit/response-delay.spec.js
@@ -13,28 +13,28 @@ describe('Response delay', function () {
 
   describe('No delay', function () {
     it('should have no delay by default', function () {
-      assert(responseDelay.delayTimeInMs() === 0);
+      assert.equal(responseDelay.delayTimeInMs(), 0);
     });
 
     it('should have no delay when given null parameter', function () {
-      assert(responseDelay.delayTimeInMs(null) === 0);
+      assert.equal(responseDelay.delayTimeInMs(null), 0);
     });
 
     it('should have no delay when given a parameter that coerce to false', function () {
-      assert(responseDelay.delayTimeInMs(false) === 0);
-      assert(responseDelay.delayTimeInMs(0) === 0);
-      assert(responseDelay.delayTimeInMs('') === 0);
-      assert(responseDelay.delayTimeInMs(NaN) === 0);
+      assert.equal(responseDelay.delayTimeInMs(false), 0);
+      assert.equal(responseDelay.delayTimeInMs(0), 0);
+      assert.equal(responseDelay.delayTimeInMs(''), 0);
+      assert.equal(responseDelay.delayTimeInMs(NaN), 0);
     });
   });
 
   describe('Simple delays', function () {
     it('should have a simple delay when given a number', function () {
-      assert(responseDelay.delayTimeInMs(42) === 42);
+      assert.equal(responseDelay.delayTimeInMs(42), 42);
     });
 
     it('should have a simple delay when given a string that coerce to a number', function () {
-      assert(responseDelay.delayTimeInMs('42') === 42);
+      assert.equal(responseDelay.delayTimeInMs('42'), 42);
     });
   });
 
@@ -48,19 +48,19 @@ describe('Response delay', function () {
       });
 
       it('should have minimum delay of 500ms when using auto parameter', function () {
-        assert(responseDelay.delayTimeInMs('auto') === 500);
+        assert.equal(responseDelay.delayTimeInMs('auto'), 500);
       });
 
       it('should have minimum delay of 150ms when using fast parameter', function () {
-        assert(responseDelay.delayTimeInMs('fast') === 150);
+        assert.equal(responseDelay.delayTimeInMs('fast'), 150);
       });
 
       it('should have minimum delay of 1500ms when using slow parameter', function () {
-        assert(responseDelay.delayTimeInMs('slow') === 1500);
+        assert.equal(responseDelay.delayTimeInMs('slow'), 1500);
       });
 
       it('should have minimum delay of 1ms when given an invalid parameter', function () {
-        assert(responseDelay.delayTimeInMs('invalid parameter') === 1);
+        assert.equal(responseDelay.delayTimeInMs('invalid parameter'), 1);
       });
     });
 
@@ -73,19 +73,19 @@ describe('Response delay', function () {
       });
 
       it('should have maximum delay of 1750ms when using auto parameter', function () {
-        assert(responseDelay.delayTimeInMs('auto') === 1750);
+        assert.equal(responseDelay.delayTimeInMs('auto'), 1750);
       });
 
       it('should have maximum delay of 1000ms when using fast parameter', function () {
-        assert(responseDelay.delayTimeInMs('fast') === 1000);
+        assert.equal(responseDelay.delayTimeInMs('fast'), 1000);
       });
 
       it('should have maximum delay of 3000ms when using slow parameter', function () {
-        assert(responseDelay.delayTimeInMs('slow') === 3000);
+        assert.equal(responseDelay.delayTimeInMs('slow'), 3000);
       });
 
       it('should have maximum delay of 1ms when given an invalid parameter', function () {
-        assert(responseDelay.delayTimeInMs('invalid parameter') === 1);
+        assert.equal(responseDelay.delayTimeInMs('invalid parameter'), 1);
       });
     });
 
@@ -115,7 +115,7 @@ describe('Response delay', function () {
       };
       responseDelay = new ResponseDelay(fakeRandomNumberGenerator);
 
-      assert(responseDelay.delayTimeInMs('fast') === 150);
+      assert.equal(responseDelay.delayTimeInMs('fast'), 150);
     });
   });
 });
